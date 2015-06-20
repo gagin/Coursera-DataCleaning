a2f60c286fed0c41bdc7af9d852b3e10f9d142a9
Alex Gaggin <alexander@gagin.ru>
2015-06-20 12:56:54 -0700

    Changed data paths to read from UCI HAR Dataset directory

--- run_analysis.R
+++ run_analysis.R
@@ -7,17 +7,19 @@ library(tidyr)
 
 # 1.1 Bind subject number and activity number
 
-# Read it all from test, put numbers for subject and activity to the first two columns, clean up memory
-test_subjects<-read.table("test/subject_test.txt",col.names="subject")
-test_Y<-read.table("test/y_test.txt",col.names="activity_number")
-test_X<-read.table("test/X_test.txt")
+# Read it all from test
+test_subjects<-read.table(file.path("UCI HAR Dataset","test","subject_test.txt"),col.names="subject")
+test_Y<-read.table(file.path("UCI HAR Dataset","test","y_test.txt"),col.names="activity_number")
+test_X<-read.table(file.path("UCI HAR Dataset","test","X_test.txt"))
+# Put numbers for subject and activity to the first two columns
 test_all<-cbind(test_subjects,test_Y,test_X)
+# Clean up the memory
 rm(test_subjects,test_X,test_Y)
 
 # Same for train
-train_subjects<-read.table("train/subject_train.txt",col.names="subject")
-train_Y<-read.table("train/y_train.txt",col.names="activity_number")
-train_X<-read.table("train/X_train.txt")
+train_subjects<-read.table(file.path("UCI HAR Dataset","train","subject_train.txt"),col.names="subject")
+train_Y<-read.table(file.path("UCI HAR Dataset","train","y_train.txt"),col.names="activity_number")
+train_X<-read.table(file.path("UCI HAR Dataset","train","X_train.txt"))
 train_all<-cbind(train_subjects,train_Y,train_X)
 rm(train_subjects,train_X,train_Y)
 
@@ -28,7 +30,7 @@ rm(test_all,train_all)
 ## Step 2. Extract only the measurements on the mean and standard deviation for each measurement.
 
 # Read features file
-features<-read.table("features.txt")
+features<-read.table(file.path("UCI HAR Dataset","features.txt"))
 
 # Keep only straightforward std and mean features
 goodfeatures<-filter(features,grepl("mean()",V2,fixed=TRUE) | grepl("std()",V2,fixed=TRUE))
@@ -40,7 +42,7 @@ gooddata<-data[,goodcolumns]
 
 ## Step 3. Use descriptive activity names to name the activities in the data set
 
-activities_labels<-read.table("activity_labels.txt")
+activities_labels<-read.table(file.path("UCI HAR Dataset","activity_labels.txt"))
 # Simpler to use as a vector, so unlist
 labels<-unlist(activities_labels$V2)
 gooddata<-mutate(gooddata,activity_number=labels[activity_number])
